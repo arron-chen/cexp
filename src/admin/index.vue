a3c5e4fb5ca5977be0e1cb1fe9ce3b5336898956
chentao <1161735119@qq.com>
2018-05-21 11:53:41 +0800

    调整结构

---
+++ src/admin/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+  export default {}
+</script>
+<style>
+  @import '../assets/less/admin/index.less';
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <Button @click="modal3 = true">No title bar</Button>
+      <Modal v-model="modal3">
+        <p>Content of dialog</p>
+        <p>Content of dialog</p>
+        <p>Content of dialog</p>
+      </Modal>
+    </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        modal2: false,
+        modal_loading: false,
+        modal3: false,
+        modal4: false,
+        modal5: false
+      }
+    },
+    methods: {
+      del () {
+        this.modal_loading = true;
+        setTimeout(() => {
+          this.modal_loading = false;
+          this.modal2 = false;
+          this.$Message.success('Successfully delete');
+        }, 2000);
+      }
+    }
+  }
+</script>
+<style>
+  @import '../assets/less/default/index.less';
+</style>
